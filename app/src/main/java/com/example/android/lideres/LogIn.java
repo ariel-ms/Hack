0e78345c3cbb9ee8c2f7cac7dbaeedd127d7115f
Ariel <ar.i117@hotmail.com>
2016-08-28 00:31:07 -0500

    Agregar registro en interfaz login

--- app/src/main/java/com/example/android/lideres/LogIn.java
+++ app/src/main/java/com/example/android/lideres/LogIn.java
@@ -1,13 +1,32 @@
 package com.example.android.lideres;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class LogIn extends AppCompatActivity {
+public class LogIn extends AppCompatActivity implements View.OnClickListener{
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+
+        switch(view.getId()){
+            case R.id.login:
+
+                break;
+        }
     }
 }
