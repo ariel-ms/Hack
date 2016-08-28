7bf77191d83a30cb6f8615a4fe25532059cdb9f8
Ariel <ar.i117@hotmail.com>
2016-08-28 05:21:39 -0500

    Spinner funcionando

--- app/src/main/java/com/example/android/lideres/Perfil.java
+++ app/src/main/java/com/example/android/lideres/Perfil.java
@@ -4,21 +4,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
-public class Perfil extends AppCompatActivity implements View.OnClickListener{
+public class Perfil extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     Button servicios;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
-        servicios = (Button) findViewById(R.id.botonServicios);
-        servicios.setOnClickListener(this);
+        //servicios = (Button) findViewById(R.id.botonServicios);
+        spinner = (Spinner) findViewById(R.id.niveles);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.levelarray, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        //servicios.setOnClickListener(this);
+        spinner.setOnItemSelectedListener(this);
     }
 
-    @Override
+    /*@Override
     public void onClick(View v) {
         Intent intent;
         switch (v.getId()){
@@ -27,5 +37,25 @@ public class Perfil extends AppCompatActivity implements View.OnClickListener{
                 startActivity(intent);
                 break;
         }
+    }*/
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Intent intent;
+        if(adapterView.getItemAtPosition(i).equals("Nivel 1")){
+            intent = new Intent(Perfil.this,Nivel.class);
+            startActivity(intent);
+        }else if (adapterView.getItemAtPosition(i).equals("Nivel 2")){
+            intent = new Intent(Perfil.this,Nivel2.class);
+            startActivity(intent);
+        }else if (adapterView.getItemAtPosition(i).equals("Nivel 3")){
+            intent = new Intent(Perfil.this,Nivel3.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
